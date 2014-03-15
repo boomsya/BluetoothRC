5f0a3a150751d3ad49d2e3049aa2c931a80e739e
robotsrulz <robots.rulz@gmail.com>
2014-03-16 02:10:26 +0400

    changed sketch for stepper motor

--- sketch/car_v1/car_v1.ino
+++ sketch/car_v1/car_v1.ino
@@ -1,5 +1,6 @@
 
 #include <Servo.h>
+#include <Stepper.h>
 
 Servo servoSteering;
 const int servoSignalPin = 7; // servo yellow(signal) pin
@@ -12,21 +13,26 @@ const int servoRightBound = servoMiddle + servoMaxAngle;
 int servoPos = servoMiddle;
 int servoTrim = 0;
 
-const int dcMotorA1Pin = 11; // dc motor A+ pin
-const int dcMotorA2Pin = 12; // dc motor A- pin
-const int dcMotorPWMPin = 3; // dc motor PWM pin
+const int ledPin = 13;
 
-const int ledPin = 13; // data received pin
+const int stepperPin1 = 11;
+const int stepperPin2 = 12;
+const int stepperPin3 = 3;
+const int stepperPin4 = 4;
 
-const int dcMotorPWMLowerBound = 128; // dc motor min PWM (min speed)
-const int dcMotorPWMHighBound = 255; // dc motor max PWM (max speed)
+const float motorSteps = 360 / 3.75;
 
-int dcMotorDir = 1; // 1 - forward, -1 - backward
-int dcMotorSpd = 0; // 0 - stand still, value between dcMotorPWMLowerBound and dcMotorPWMHighBound to move
+Stepper myStepper(motorSteps, stepperPin1, stepperPin2, stepperPin3, stepperPin4);
+
+const int stepperLowerBound = 20; // dc motor min PWM (min speed)
+const int stepperHighBound = 200; // dc motor max PWM (max speed)
+
+int stepperDir = 1; // 1 - forward, -1 - backward
+int stepperSpd = 0; // 0 - stand still, value between stepperLowerBound and stepperHighBound to move
 
 const int cmdTimeout = 2000; // max time between commands from rc control, will stop if no new commands arrive
 
-typedef enum { NONE = 0, SERVO_L, SERVO_R, TRIM, DCMOTOR_F, DCMOTOR_B, STOP } mode_t;
+typedef enum { NONE = 0, SERVO_L, SERVO_R, TRIM, stepper_F, stepper_B, STOP } mode_t;
 mode_t opMode = NONE;
 
 #define IS_DIGIT(x) ('9' >= (x) && '0' <= (x))
@@ -37,20 +43,10 @@ void setup() {
 
   Serial.begin(9600);
 
-// https://github.com/arduino/Arduino/issues/672
-//  Serial.setBlocking(0);
-
   servoSteering.attach(servoSignalPin);
 
-  pinMode(dcMotorA1Pin, OUTPUT);
-  pinMode(dcMotorA2Pin, OUTPUT);
-  pinMode(dcMotorPWMPin, OUTPUT);
-
+  myStepper.setSpeed(10);
   pinMode(ledPin, OUTPUT);
-
-  digitalWrite(dcMotorA1Pin, 0);
-  digitalWrite(dcMotorA2Pin, 0);
-  analogWrite(dcMotorPWMPin, 0);
 }
 
 void loop() {
@@ -78,17 +74,17 @@ void loop() {
           break;
         case 'F':
         case 'f': // dc motor move front mode
-          opMode = DCMOTOR_F;
+          opMode = stepper_F;
           break;
         case 'B':
         case 'b': // dc motor move back mode
-          opMode = DCMOTOR_B;
+          opMode = stepper_B;
           break;
         case 'S':
         case 's': // full stop mode
           servoPos = servoMiddle + servoTrim; // center front wheels
-          dcMotorDir = 1; // 1st gear
-          dcMotorSpd = 0; // neutral on
+          stepperDir = 1; // 1st gear
+          stepperSpd = 0; // neutral on
         default:
           opMode = NONE;
           break;
@@ -106,13 +102,13 @@ void loop() {
           servoPos = servoMiddle + servoTrim + (ch - '0') * servoMaxAngle / 10;
           if (servoPos > servoRightBound) servoPos = servoRightBound;
           break;
-        case DCMOTOR_F:
-          dcMotorDir = 1;
-          dcMotorSpd = ch == '0' ? 0 : dcMotorPWMLowerBound + (dcMotorPWMHighBound - dcMotorPWMLowerBound) * (ch - '0') / 10;
+        case stepper_F:
+          stepperDir = 1;
+          stepperSpd = ch == '0' ? 0 : stepperLowerBound + (stepperHighBound - stepperLowerBound) * (ch - '0') / 10;
           break;
-        case DCMOTOR_B:
-          dcMotorDir = -1;
-          dcMotorSpd = ch == '0' ? 0 : dcMotorPWMLowerBound + (dcMotorPWMHighBound - dcMotorPWMLowerBound) * (ch - '0') / 10;
+        case stepper_B:
+          stepperDir = -1;
+          stepperSpd = ch == '0' ? 0 : stepperLowerBound + (stepperHighBound - stepperLowerBound) * (ch - '0') / 10;
           break;
         default:
           break;
@@ -121,36 +117,33 @@ void loop() {
       opMode = NONE;
     }
 
-    if (dcMotorDir == 1)
+    if (stepperDir == 1)
       Serial.print("F ");
     else
       Serial.print("R ");
 
-    Serial.print(dcMotorSpd);
+    Serial.print(stepperSpd);
     Serial.print(" S ");
     Serial.print(servoPos);
     Serial.print(" T ");
     Serial.println(servoTrim);
 
-    digitalWrite(dcMotorA1Pin, dcMotorDir == 1 ? 1 : 0);
-    digitalWrite(dcMotorA2Pin, dcMotorDir == 1 ? 0 : 1);
-    analogWrite(dcMotorPWMPin, dcMotorSpd);
-
     servoSteering.write(servoPos);
   }
   
-  if (dcMotorSpd > 0 && (lastCmd  + cmdTimeout) < millis()) {
+  if (stepperSpd > 0 && (lastCmd  + cmdTimeout) < millis()) {
 
     // rc control doesn't send anything, out of range or hang up, do full stop
-    dcMotorDir = 1; // 1st gear
-    dcMotorSpd = 0; // neutral on
-
-    digitalWrite(dcMotorA1Pin, 1);
-    digitalWrite(dcMotorA2Pin, 0);
-    analogWrite(dcMotorPWMPin, 0);
+    stepperDir = 1; // 1st gear
+    stepperSpd = 0; // neutral on
 
     servoSteering.write(servoPos);
   }
+
+   if (stepperSpd > 0) {
+      myStepper.setSpeed(stepperSpd);
+      myStepper.step(1 * stepperDir);
+    }
 
   digitalWrite(ledPin, LOW);
 }
